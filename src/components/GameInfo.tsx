439113fcb1fbf24e13567c5520892403b3326e7e
Ferdy Budhidharma <ferd312@gmail.com>
2017-08-11 15:05:39 -0500

    Added tie detection

--- src/components/GameInfo.tsx
+++ src/components/GameInfo.tsx
@@ -2,11 +2,15 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import MoveLink from './MoveLink';
 
+const isBoardFilled = (state: GameState) => state.winner == null && state.moves.length === state.size ** 2 + 1;
+
 function GameInfo(props: GameState) {
     return (
         <div className="game-info">
             <div>
-                {props.winner ? `Winning player: ${props.winner}` : `Next player: ${props.nextPlayer}`}
+                {props.winner
+                    ? `Winning player: ${props.winner}`
+                    : isBoardFilled(props) ? `It's a tie!` : `Next player: ${props.nextPlayer}`}
             </div>
             <ol>
                 {props.moves.map((val, idx) =>
